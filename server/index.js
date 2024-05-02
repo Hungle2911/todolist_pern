3d5888393b9ed609a7d3fb2653caa1f311f50836
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-05-02 15:56:45 -0400

    feat: add function that handle deleting a todo on server side

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.post('/todos', async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query('INSERT INTO todo(description) VALUES ($1) RETURNING *', [description])
-    console.log(newTodo.rows);
-    // console.log(res.json(newTodo.rows));
+    // console.log(newTodo.rows);
+    res.json(newTodo.rows[0]);
   } catch (err){
     console.error(err.message);
   }
@@ -34,11 +34,38 @@ app.get('/todos', async (req, res) => {
   }
 })
 //get a todo
-
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const {id} = req.param;
+    const todo = await pool.query('SELECT * FROM todo WHERE id = $1', [id])
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 //update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(``, [])
+  } catch (error) {
+    console.error(err.message);
+  }
+})
 
 //delete a todo
-
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 app.listen(PORT, () => {console.log(`Server has started on port ${PORT}`);})
 
 module.exports = {PORT}
